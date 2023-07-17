a9fc25b023bb9fee8a760dea4b1ce5e90693d654
Phuchon Chueapong <128043114+PhuCholony@users.noreply.github.com>
2023-07-17 22:28:34 +0700

    setup dotenv dependency
    https://github.com/dotenv-rs/dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
+    // Load DotENV file.
+    dotenv::dotenv().ok();
+
+    // Start rocket server.
     server::serve().launch().await?;
+
     Ok(())
 }
